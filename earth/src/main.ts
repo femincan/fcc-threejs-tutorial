0f3094e40b9e1bfc23910961251a07968ae7fa30
Furkan Emin Can <femincanx@gmail.com>
2024-07-28 23:49:44 +0300

    feat(earth): add earth with stars

--- earth/src/main.ts
+++ earth/src/main.ts
@@ -1,12 +1,13 @@
 import * as t from 'three';
 import './style.css';
 import { TrackballControls } from 'three/addons/controls/TrackballControls.js';
+import { getStarfield } from './utils';
 
 const w = document.body.clientWidth;
 const h = document.body.clientHeight;
 const scene = new t.Scene();
 const camera = new t.PerspectiveCamera(75, w / h, 0.1, 1000);
-camera.position.z = 5;
+camera.position.z = 4;
 
 const renderer = new t.WebGLRenderer({ antialias: true });
 renderer.setSize(w, h);
@@ -14,21 +15,32 @@ renderer.setSize(w, h);
 document.body.appendChild(renderer.domElement);
 
 const controls = new TrackballControls(camera, renderer.domElement);
-controls.rotateSpeed = 10;
+controls.rotateSpeed = 1;
+
+const earthGroup = new t.Group();
+earthGroup.rotation.z = (-23.4 * Math.PI) / 100;
+scene.add(earthGroup);
+
+const loader = new t.TextureLoader();
 
 const geometry = new t.IcosahedronGeometry(1, 12);
 const material = new t.MeshStandardMaterial({
-  color: 0xffff00,
+  map: loader.load('/earth-map.jpg'),
 });
 const earthMesh = new t.Mesh(geometry, material);
-scene.add(earthMesh);
+earthGroup.add(earthMesh);
 
-const hemiLight = new t.HemisphereLight(0xffffff, 0x444444);
-scene.add(hemiLight);
+const stars = getStarfield();
+scene.add(stars);
+
+const sunLight = new t.DirectionalLight(0xffffff);
+sunLight.position.set(-2, 0.5, 1);
+scene.add(sunLight);
 
 function animate() {
   renderer.render(scene, camera);
   controls.update();
+  earthMesh.rotation.y += 0.002;
 
   requestAnimationFrame(animate);
 }
---
+++ earth/src/utils.ts
@@ -0,0 +1,43 @@
+import * as t from 'three';
+
+export function getStarfield({ numStars = 500 } = {}) {
+  function randomSpherePoint() {
+    const radius = Math.random() * 25 + 25;
+    const u = Math.random();
+    const v = Math.random();
+    const theta = 2 * Math.PI * u;
+    const phi = Math.acos(2 * v - 1);
+    let x = radius * Math.sin(phi) * Math.cos(theta);
+    let y = radius * Math.sin(phi) * Math.sin(theta);
+    let z = radius * Math.cos(phi);
+
+    return {
+      pos: new t.Vector3(x, y, z),
+      hue: 0.6,
+      minDist: radius,
+    };
+  }
+  const verts = [];
+  const colors = [];
+  const positions = [];
+  let col;
+  for (let i = 0; i < numStars; i += 1) {
+    let p = randomSpherePoint();
+    const { pos, hue } = p;
+    positions.push(p);
+    col = new t.Color().setHSL(hue, 0.2, Math.random());
+    verts.push(pos.x, pos.y, pos.z);
+    colors.push(col.r, col.g, col.b);
+  }
+  const geo = new t.BufferGeometry();
+  geo.setAttribute('position', new t.Float32BufferAttribute(verts, 3));
+  geo.setAttribute('color', new t.Float32BufferAttribute(colors, 3));
+  const mat = new t.PointsMaterial({
+    size: 0.2,
+    vertexColors: true,
+    map: new t.TextureLoader().load('/circle.png'),
+  });
+  const points = new t.Points(geo, mat);
+  return points;
+}
+
