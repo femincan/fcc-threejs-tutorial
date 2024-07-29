1ae324f12e905bb41e1ba595aad7592f4e614ddc
Furkan Emin Can <femincanx@gmail.com>
2024-07-29 23:44:32 +0300

    feat(earth): add new features

--- earth/src/main.ts
+++ earth/src/main.ts
@@ -1,11 +1,20 @@
 import * as t from 'three';
 import './style.css';
-import { TrackballControls } from 'three/addons/controls/TrackballControls.js';
-import { getStarfield } from './utils';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
-const w = document.body.clientWidth;
-const h = document.body.clientHeight;
+const w = window.innerWidth;
+const h = window.innerHeight;
 const scene = new t.Scene();
+scene.background = new t.CubeTextureLoader()
+  .setPath('/background/')
+  .load([
+    'right.png',
+    'left.png',
+    'top.png',
+    'bottom.png',
+    'front.png',
+    'back.png',
+  ]);
 const camera = new t.PerspectiveCamera(75, w / h, 0.1, 1000);
 camera.position.z = 4;
 
@@ -14,8 +23,8 @@ renderer.setSize(w, h);
 
 document.body.appendChild(renderer.domElement);
 
-const controls = new TrackballControls(camera, renderer.domElement);
-controls.rotateSpeed = 1;
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
 
 const earthGroup = new t.Group();
 earthGroup.rotation.z = (-23.4 * Math.PI) / 100;
@@ -23,15 +32,19 @@ scene.add(earthGroup);
 
 const loader = new t.TextureLoader();
 
-const geometry = new t.IcosahedronGeometry(1, 12);
+const geometry = new t.IcosahedronGeometry(1, 16);
 const material = new t.MeshStandardMaterial({
   map: loader.load('/earth-map.jpg'),
 });
 const earthMesh = new t.Mesh(geometry, material);
 earthGroup.add(earthMesh);
 
-const stars = getStarfield();
-scene.add(stars);
+const nightMat = new t.MeshBasicMaterial({
+  map: loader.load('/earth-night-map.jpg'),
+  blending: t.AdditiveBlending,
+});
+const nightMesh = new t.Mesh(geometry, nightMat);
+earthGroup.add(nightMesh);
 
 const sunLight = new t.DirectionalLight(0xffffff);
 sunLight.position.set(-2, 0.5, 1);
@@ -39,10 +52,20 @@ scene.add(sunLight);
 
 function animate() {
   renderer.render(scene, camera);
-  controls.update();
-  earthMesh.rotation.y += 0.002;
 
-  requestAnimationFrame(animate);
+  controls.update();
+
+  earthGroup.rotation.y += 0.002;
 }
 
-animate();
+renderer.setAnimationLoop(animate);
+
+window.addEventListener('resize', () => {
+  const w = window.innerWidth;
+  const h = window.innerHeight;
+
+  camera.aspect = w / h;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize(w, h);
+});
